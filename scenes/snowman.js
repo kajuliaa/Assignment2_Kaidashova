2dd63ef176f56dbb5dcff00c6325626dfffdba39
Julia Kaidashova <julia.kaidashova5@gmail.com>
2023-11-24 20:05:49 +0100

    Snowman added

---
+++ scenes/snowman.js
@@ -0,0 +1,56 @@
+{
+  "eye": [0,1,15],
+  "lookat": [-1,1,-10],
+  "up": [0,1,0],
+  "fov_angle": 60,
+  "width": 480,
+  "height": 270,
+  "surfaces": [
+    {
+      "type": "sphere",
+      "center": [0,1,-5],
+      "radius": 3.5,
+      "ambient": [0.3,0.75,0.4],
+      "diffuse": [0.95294117647,0.96470588235,0.98431372549],
+      "specular": [0.2,0.2,0.2],
+      "mirror": [0.1,0.1,0.1],
+      "phong_exponent": 15
+    },
+    {
+      "type": "sphere",
+      "center": [0,5,-5],
+      "radius": 2,
+      "ambient": [0.75,0.75,0.75],
+      "diffuse": [0.95294117647,0.96470588235,0.98431372549],
+      "specular": [0.2,0.2,0.2],
+      "mirror": [0,0,0],
+      "phong_exponent": 15
+    },
+    {
+      "type": "sphere",
+      "center": [0,7.5,-5],
+      "radius": 1.25,
+      "ambient": [0.75,0.3,0.4],
+      "diffuse": [0.95294117647,0.96470588235,0.98431372549],
+      "specular": [0.2,0.2,0.2],
+      "mirror": [0,0,0],
+      "phong_exponent": 15
+    },
+    {
+      "type": "plane",
+      "point": [0,-3,-10],
+      "normal": [0,1,0],
+      "ambient": [0.025,0.025,0.05],
+      "diffuse": [0.505,0.321,0.176],
+      "specular": [0.0,0.0,0.0],
+      "mirror": [0.0,0.0,0.0],
+      "phong_exponent": 0
+    }
+  ],
+ "lights": [
+    {
+      "position": [30,45,10],
+      "color": [0.8,0.8,0.8]
+    }
+  ]
+}
